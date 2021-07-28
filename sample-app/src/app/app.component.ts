ae27bbff339a6a44c42f46636074f17ab219b409
Ronny Soutart <soutartronny@gmail.com>
2021-07-28 22:58:09 +1100

    feat(app): Minors changes

--- sample-app/src/app/app.component.ts
+++ sample-app/src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   title = 'sample-app';
   active = 1;
 
-  // icon
+  // icons
   faUser = faUser;
   faUserCircle = faUserCircle;
   faCalendar = faCalendar;
@@ -19,12 +19,16 @@ export class AppComponent {
 
   // users
   users: IUser[] = [
-    { userId: 1, firstName: 'John', name: 'Doe', birthDate: '2001-04-02', points: 76137613, email: 'denial.roast@unknown.com' },
-    { userId: 2, firstName: 'Jane', name: 'Doe', birthDate: '1986-12-06', points: 7467, email: 'devid.neo@unknown.com' },
-    { userId: 3, firstName: 'Jean', name: 'Dupont', birthDate: '1972-09-19', points: 987559, email: 'krish.lee@caramail.fr' },
-    { userId: 4, firstName: 'Xiao', name: 'Lan', birthDate: '2010-03-21', points: 16394, email: 'racks.jacson@middle.cn' },
-    { userId: 5, firstName: 'Marc', name: 'Milian', birthDate: '2004-10-17', points: 157474746, email: 'jone.mac@gmail.com' },
-    { userId: 6, firstName: 'Fabrice', name: 'Hubert', birthDate: '1956-05-01', points: 0, email: 'fab@yahoo.com' },
+    { userId: 1, firstName: 'John', name: 'Doe', birthDate: '2001-04-02', points: 76137613, email: 'denial.roast@unknown.com', photoUrl: "https://randomuser.me/api/portraits/men/79.jpg" },
+    { userId: 2, firstName: 'Jane', name: 'Doe', birthDate: '1986-12-06', points: 7467, email: 'devid.neo@unknown.com', photoUrl: "https://randomuser.me/api/portraits/women/44.jpg" },
+    { userId: 3, firstName: 'Jean', name: 'Dupont', birthDate: '1972-09-19', points: 987559, email: 'krish.lee@caramail.fr', photoUrl: "https://randomuser.me/api/portraits/men/54.jpg" },
+    { userId: 4, firstName: 'Xiao', name: 'Lan', birthDate: '2010-03-21', points: 16394, email: 'racks.jacson@middle.cn', photoUrl: "https://randomuser.me/api/portraits/men/13.jpg" },
+    { userId: 5, firstName: 'Marc', name: 'Milian', birthDate: '2004-10-17', points: 157474746, email: 'jone.mac@gmail.com', photoUrl: "https://randomuser.me/api/portraits/men/11.jpg" },
+    { userId: 6, firstName: 'Fabienne', name: 'Hubert', birthDate: '1956-05-01', points: 0, email: 'fab@yahoo.com', photoUrl: "https://randomuser.me/api/portraits/women/17.jpg" },
+    { userId: 7, firstName: 'Nathan', name: 'Bell', birthDate: '1985-06-08', points: 527935, email: 'nathan.bell@example.com', photoUrl: "https://randomuser.me/api/portraits/men/26.jpg" },
+    { userId: 8, firstName: 'Don', name: 'Peters', birthDate: '1979-03-06', points: 0, email: 'don.peters@yahoo.com', photoUrl: "https://randomuser.me/api/portraits/men/15.jpg" },
+    { userId: 9, firstName: 'Roger', name: 'Myers', birthDate: '1954-04-04', points: 0, email: 'roger.myers@example.com', photoUrl: "https://randomuser.me/api/portraits/men/32.jpg" },
+    { userId: 10, firstName: 'Stephanie', name: 'Lane', birthDate: '1947-03-02', points: 0, email: 'stephanie.lane@gmail.com', photoUrl: "https://randomuser.me/api/portraits/women/35.jpg" },
   ]
 
 }
@@ -36,4 +40,5 @@ export interface IUser {
   birthDate: string;
   points: number;
   email: string;
+  photoUrl: string
 }
